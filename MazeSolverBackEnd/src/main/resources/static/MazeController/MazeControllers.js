68402335278bfdef5045408040d563d7adb091dd
Ryan Grewatz <grewatzmail@gmail.com>
2017-08-20 00:24:19 -0500

    Fixing final bugs in prototype app. Still need to remove debugging way of firing onchange listener.

--- MazeSolverBackEnd/src/main/resources/static/MazeController/MazeControllers.js
+++ MazeSolverBackEnd/src/main/resources/static/MazeController/MazeControllers.js
@@ -5,19 +5,15 @@ angular.module('MazeControllers', ['ngRoute'])
         function($scope, $http) {
             $scope.rows = 0;
             $scope.columns = 0;
-            $scope.textAreaText = '';
             $scope.mazeText = '';
             $scope.maze = [];
-            $scope.solveOrRefresh = 'Solve';
+            $scope.blah = null;
             $scope.getArrayOfSize = function(x){
                 return new Array(x);
             };
 
-            $scope.textChanged = function(){
-                $scope.populateMazeFromText($scope.textAreaText);
-            };
-
             $scope.populateMazeFromText = function(text){
+                $scope.maze = [];
                 var mazeRows = text.split('\n');
                 for(var i = 0; i < mazeRows.length; i++){
                     var cells = [];
@@ -27,28 +23,57 @@ angular.module('MazeControllers', ['ngRoute'])
                     $scope.maze.push(cells);
                 }
             };
+
             $scope.solve = function(){
-                $scope.maze = [];
-                if($scope.solveOrRefresh == 'Solve'){
+                if($scope.mazeText != '') {
                     $http({
-                        method : "POST",
-                        url : "http://localhost:8080/solve",
+                        method: "POST",
+                        url: "http://localhost:8080/solve",
                         headers: {
                             'Content-Type': 'text/plain'
                         },
-                        data : $scope.textAreaText,
+                        data: $scope.mazeText,
                         transformResponse: [function (data) {
                             return data;
-                        }]                    
+                        }]
                     }).then(function mySuccess(response) {
                         $scope.populateMazeFromText(response.data);
                     }, function myError(response) {
                         $scope.maze = response.statusText;
                     });
-                    $scope.solveOrRefresh = 'Refresh'
                 }
                 else{
-                    location.reload();
+                    $scope.alert("Please upload a maze for solving.")
                 }
+            };
+
+            $scope.parseFile = function(){
+                var f = event.target.files[0],
+                    r = new FileReader();
+
+                r.onloadend = function(e) {
+                    $scope.mazeText = e.target.result;
+                    $scope.populateMazeFromText($scope.mazeText);
+                    $scope.$apply();
+                    //send your binary data via $http or $resource or do anything else with it
+                }
+
+                r.readAsText(f);
+            };
+        }
+    ])
+    .directive('dataFile', function() {
+        return {
+            require:"ngModel",
+            restrict: 'A',
+            link: function($scope, el, attrs, ngModel){
+                el.bind('change', function(event){
+                    var files = event.target.files;
+                    var file = files[0];
+
+                    ngModel.$setViewValue(file);
+                    $scope.$apply();
+                });
             }
-    }]);
+        };
+    });
