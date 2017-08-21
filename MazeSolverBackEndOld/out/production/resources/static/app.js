84a5578d20bf12eff5ec63d34111f2cd3d2bcc8a
Ryan Grewatz <grewatzmail@gmail.com>
2017-08-20 22:23:51 -0500

    Removing old named mazesolver

--- MazeSolverBackEndOld/out/production/resources/static/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('mazeApp', ['ngRoute','MazeControllers'])
-    .config(['$locationProvider', '$routeProvider', function($locationProvider) {
-    $locationProvider.hashPrefix('!');
-}]);
--- MazeSolverBackEndOld/src/main/java/com/peoplenet/mazesolver/Solver/MazeSolvingAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package com.peoplenet.mazesolver.Solver;
-
-/**
- * Created by grewa on 8/12/2017.
- */
-public interface MazeSolvingAlgorithm {
-    public Vertex execute(char[][] maze, Vertex start, Vertex end);
-}
